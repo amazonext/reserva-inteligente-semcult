23d493f50e240f92ef31d2eebc8c0c2ef8c8c1ec
Ed <108343328+eded001@users.noreply.github.com>
2025-11-18 14:52:41 -0300

    feat: add react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,20 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-export default function App() {
-  const [status, setStatus] = useState({
-    online: false,
-    message: "Test pending",
-  });
+import Home from "@/pages/Home";
+import Register from "@/pages/Register";
+import Login from "@/pages/Login";
 
-  const testBackendConnection = async () => {
-    try {
-      const response = await fetch("/api/test");
-      if (!response.ok) {
-        setStatus({ online: false, message: "Offline" });
-        return;
-      }
-
-      const data = await response.json();
-      console.log("Resposta do backend:", data);
-      setStatus({ online: true, message: "Online" });
-    } catch (error) {
-      console.error("Falha na requisição:", error);
-      setStatus({ online: false, message: "Offline" });
-    }
-  }
-
-  const statusColor =
-    status.message === "Test pending"
-      ? "text-yellow-600"
-      : status.online
-        ? "text-green-600"
-        : "text-red-600";
-
+const AppRoutes = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-4">
-      <Button onClick={testBackendConnection}>
-        Testar Conexão com o Backend
-      </Button>
-
-      <p className="text-lg font-medium absolute bottom-5">
-        Server status: <span className={statusColor}>{status.message}</span>
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default AppRoutes;
