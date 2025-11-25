a37c67d0784462f16de92e5470990d9a8e59e33c
LiviaPC <livia.p.cardoso@outlook.com>
2025-11-25 16:14:13 -0300

    feat: criacao de pagina de gestao de reservas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@/pages/Home";
 import Dashboard from "@/pages/Dashboard";
+import Reservations from "@/pages/Reservations";
 
 const AppRoutes = () => {
   return (
@@ -10,6 +11,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/reservations" element={<Reservations />} />
       </Routes>
     </BrowserRouter>
   );
