2ac09cbdf01824cd8053352d6be342a27ed9fe93
Ed <108343328+eded001@users.noreply.github.com>
2025-11-26 22:43:01 -0300

    chore: change the table

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,6 @@
 from supabase import create_client, Client
 from app.core.config import SUPABASE_URL, SUPABASE_KEY
-from requests.exceptions import RequestException  # type: ignore
+from requests.exceptions import RequestException
 
 # Códigos de cores ANSI
 GREEN = "\033[92m"
@@ -10,7 +10,7 @@ RESET = "\033[0m"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 try:
-    if supabase.table("test").select("*").limit(1).execute().data is not None:
+    if supabase.table("profiles").select("*").limit(1).execute().data is not None:
         print(f"{GREEN}Conexão com Supabase estabelecida com sucesso!{RESET}")
 except RequestException as e:
     print(f"{RED}Falha ao conectar com Supabase: {e}{RESET}")
