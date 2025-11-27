8ee6ce2df18c77dd158ab85714d63058cf521b62
Lucas <129661036+lucasonline0@users.noreply.github.com>
2025-11-27 08:38:12 -0300

    fix(app): Corrige importações de rotas e o context provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@/pages/Home";
-import Dashboard from "@/components/ReservationModal";
+import Dashboard from "@/pages/UserReservation";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Reservations from "@/pages/Reservations";
 import UserReservation from "@/pages/UserReservation";
 
 // contexts
-import { AuthContext } from "./contexts/AuthContext";
+import { AuthProvider } from "./contexts/AuthContext";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <AuthContext>
+      <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
@@ -23,7 +23,7 @@ const AppRoutes = () => {
           <Route path="/reservations" element={<Reservations />} />
           <Route path="/userreservation" element={<UserReservation />} />
         </Routes>
-      </AuthContext>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
