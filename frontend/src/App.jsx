f0ff3bd9ca3e42d1324fb8ec2a46a9679c53729a
PedroHenriqueSants <11pedrohenriquesantos@gmail.com>
2025-11-27 09:49:56 -0300

    feat: add scrolltotop component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+// pages
 import Home from "@/pages/Home";
 import Dashboard from "@/pages/UserReservation";
 import About from "./pages/About";
@@ -8,12 +8,16 @@ import NotFound from "./pages/NotFound";
 import Reservations from "@/pages/Reservations";
 import UserReservation from "@/pages/UserReservation";
 
+// components
+import ScrollToTop from "./components/ScrollToTop";
+
 // contexts
 import { AuthProvider } from "./contexts/AuthContext";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
---
+++ frontend/src/components/ScrollToTop.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+// faz a janela a rolar para o topo sempre que a rota mudar
+  useEffect(() => {
+    
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
